c0737168b1c12d7773f2d5cd8a4ae9b7a569b009
GabrielVRM <gabriel.vieira2595@gmail.com>
2023-06-06 22:11:03 -0300

    att

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -41,7 +41,7 @@ export default function Video(props) {
         <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video  ">
           <iframe
             className="iframe "
-            src={data.lesson.videoId}
+            src={data?.lesson?.videoId}
             height="500px"
             scrolling="no"
           />
@@ -50,8 +50,8 @@ export default function Video(props) {
       <div className="p-8 max-w-[1100px] mx-auto">
         <div className="flex items-start gap-16">
           <div className="flex-1">
-            <h1 className="text-2xl font-bold">{data.lesson.title}</h1>
-            <p className="mt-4 text-gray-200">{data.lesson.description}</p>
+            <h1 className="text-2xl font-bold">{data?.lesson?.title}</h1>
+            <p className="mt-4 text-gray-200">{data?.lesson?.description}</p>
             <div className="flex items-center gap-4 mt-6">
               <img
                 className="h-16 w-16 rounded-full border-2 border-blue-500"
@@ -59,31 +59,31 @@ export default function Video(props) {
               ></img>
               <div className="flex flex-col items-center leading-relaxed ">
                 <strong className="font-bold text-2xl">
-                  {data.lesson.gabriel.name}
+                  {data?.lesson?.gabriel?.name}
                 </strong>
                 <span className="text-gray-400">Developer</span>
               </div>
             </div>
           </div>
           <div className="flex flex-col gap-4 ">
-            {data.lesson.lessonType === "Projetos" ? 
+            {data?.lesson?.lessonType === "Projetos" ? 
               <div  className="flex flex-col gap-4 " >
                 <Button
                   title="Acessar o Projeto"
                   icon={<GithubLogo />}
-                  link={data.lesson.videoId}
+                  link={data?.lesson?.videoId}
                 />
                 <Button
                   title="Acessar o Repositorio"
                   icon={<Code />}
-                  link={data.lesson.repositorio}
+                  link={data?.lesson?.repositorio}
                 />
               </div>
-             : 
+              : 
               <Button
                 title="Acessar o Repositorio"
                 icon={<Code />}
-                link={data.lesson.repositorio}
+                link={data?.lesson?.repositorio}
               />
             }
           </div>
--- src/lib/apollo.js
+++ src/lib/apollo.js
@@ -3,7 +3,10 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 export const client = new ApolloClient({
 
     uri: 'https://api-sa-east-1.hygraph.com/v2/clhm5tj66552e01uh8vstazt4/master',
-    cache:new InMemoryCache()
+    headers:{
+        'Authorization': `Bearer ${import.meta.env.VITE_TOKEN_MAXIMO}`
+    },
+    cache: new InMemoryCache()
 
 })
 
